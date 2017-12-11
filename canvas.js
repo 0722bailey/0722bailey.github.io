fb73448b070c18444f8d0a825070505e3559111f
0722bailey <32972349+0722bailey@users.noreply.github.com>
2017-12-10 20:43:17 -0500

    Frame per second test of circles

--- canvas.js
+++ canvas.js
@@ -5,60 +5,89 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
+// arc
+/* for (var i = 0; i < 100; i++) {
+    var x = Math.random() * innerWidth
+        ;
+    var y = Math.random() * innerHeight
+        ;
+    var colorX = Math.round(Math.random() * 255);
+    var colorY = Math.round(Math.random() * 255);
+    var colorZ = Math.round(Math.random() * 255);
+    c.strokeStyle = 'rgba('+colorX+', '+colorY+', '+colorZ+', 1)';
+    c.beginPath();
+    c.arc(x, y, 30, 0, Math.PI * 2, false);
+    c.stroke();  
+}    
+*/
 
-// ordered by y co-ordinates (x, Y, width, height)
+function Circle(x, y, dx, dy, radius, colorX, colorY, colorZ) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.colorX = colorX;
+    this.colorY = colorY;
+    this.colorZ = colorZ;
+    
+    this.draw = function() {
+        
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.strokeStyle = 'rgba('+this.colorX+', '+this.colorY+', '+this.colorZ+',         1)';
+        c.lineWidth = 5;
+        c.stroke();
+        ;
+    }
+    this.update = function() {
+        if (this.x + this.radius > innerWidth || 
+        this.x - this.radius < 0) {
+            this.dx = -this.dx;
+        }
+        if (this.y + this.radius > innerHeight || 
+        this.y - this.radius < 0) {
+            this.dy = -this.dy;
+        }
+        this.x += this.dx;
+        this.y += this.dy; 
+        
+        this.draw();
+    }
+}
 
-c.fillStyle = 'red';
-c.fillRect(50, 20, 50, 10);
-c.fillRect(40, 30, 90, 10);
-c.fillRect(60, 90, 10, 20);
-c.fillRect(90, 90, 10, 20);
-c.fillRect(60, 110, 40, 10);
-c.fillRect(50, 120, 10, 10);
-c.fillRect(70, 120, 20, 10);
-c.fillRect(100, 120, 10, 10);
-c.fillRect(50, 130, 60, 10);
-c.fillRect(40, 140, 80, 10);
-c.fillRect(40, 150, 30, 10);
-c.fillRect(90, 150, 30, 10);
+var circleArray = [];
 
-c.fillStyle = 'green'
-c.fillRect(40, 40, 30, 10);
-c.fillRect(90, 40, 10, 20);
-c.fillRect(50, 50, 10, 10);
-c.fillRect(30, 50, 10, 20);
-c.fillRect(50, 60, 20, 10);
-c.fillRect(100, 60, 10, 10);
-c.fillRect(30, 70, 20, 10);
-c.fillRect(90, 70, 40, 10);
-c.fillRect(40, 90, 20, 10);
-c.fillRect(70, 90, 20, 20);
-c.fillRect(30, 100, 30, 10);
-c.fillRect(100, 100, 30, 10)
-c.fillRect(20, 110, 40, 10);
-c.fillRect(100, 110, 40, 10);
-c.fillRect(40, 120, 10, 10);
-c.fillRect(110, 120, 10, 10);
-c.fillRect(30, 160, 30, 10);
-c.fillRect(100, 160, 30, 10);
-c.fillRect(20, 170, 40, 10);
-c.fillRect(100, 170, 40, 10);
+for(var i = 0; i < 150; i++) {
+    var radius = 30;
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
+    var dx = (Math.random() - 0.5) * 5;
+    var dy = (Math.random() - 0.5) * 5;
+    var colorX = Math.round(Math.random() * 255);
+    var colorY = Math.round(Math.random() * 255);
+    var colorZ = Math.round(Math.random() * 255);
+    circleArray.push(new Circle(x, y, dx, dy, radius, colorX, colorY, colorZ));
+}
 
-c.fillStyle = 'yellow'
-c.fillRect(70, 40, 20, 10);
-c.fillRect(100, 40, 10, 10);
-c.fillRect(40, 50, 10, 20);
-c.fillRect(60, 50, 30, 10);
-c.fillRect(100, 50, 30, 10);
-c.fillRect(70, 60, 30, 10);
-c.fillRect(110, 60, 30, 10);
-c.fillRect(50, 70, 40, 10);
-c.fillRect(50, 80, 70, 10);
-c.fillRect(20, 120, 20, 10);
-c.fillRect(60, 120, 10, 10);
-c.fillRect(90, 120, 10, 10);
-c.fillRect(120, 120, 20, 10);
-c.fillRect(20, 130, 30, 10);
-c.fillRect(110, 130, 30, 10);
-c.fillRect(20, 140, 20, 10);
-c.fillRect(120, 140, 20, 10);
+var lastLoop = performance.now();
+var fpsInterval = lastLoop;
+var fpsDisplay = 0;
+function animate() { 
+    var thisLoop = performance.now();
+    var fps = Math.round(1000 / (thisLoop - lastLoop));
+    lastLoop = thisLoop;
+    if(performance.now() - fpsInterval > 500) {
+        fpsDisplay = fps;
+        fpsInterval = performance.now();
+    }
+   requestAnimationFrame(animate); 
+    c.clearRect(0, 0, innerWidth, innerHeight);
+    c.font="20px Arial"
+    for(var i = 0; i < circleArray.length; i++) {
+        circleArray[i].update();
+    }
+    c.fillText(fpsDisplay+" FPS", 25, 25);
+}
+
+animate();
